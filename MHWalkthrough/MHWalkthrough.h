28bf81933619a18c7d734175eb7af9e6625d6393
Maiko Hermans <mwmhermans@outlook.com>
2017-03-15 17:21:57 +0100

    Initial Commit

---
+++ MHWalkthrough/MHWalkthrough.h
@@ -0,0 +1,19 @@
+//
+//  MHWalkthrough.h
+//  MHWalkthrough
+//
+//  Created by Maiko Hermans on 15/03/2017.
+//  Copyright © 2017 Maiko Hermans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MHWalkthrough.
+FOUNDATION_EXPORT double MHWalkthroughVersionNumber;
+
+//! Project version string for MHWalkthrough.
+FOUNDATION_EXPORT const unsigned char MHWalkthroughVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MHWalkthrough/PublicHeader.h>
+
+
